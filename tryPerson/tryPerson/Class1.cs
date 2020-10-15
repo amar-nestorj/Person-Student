6471ba67efccd30285ef2376c3942d5a7e639069
Nestor T. Amar Jr <72591828+amar-nestorj@users.noreply.github.com>
2020-10-15 12:07:50 +0800

    Add files via upload

---
+++ tryPerson/tryPerson/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryPerson
+{
+     class Person
+    {
+         public string firstName { get; set; }
+         public string lastName { get; set; }
+         public string suffix { get; set; }
+         public string midIn { get; set; }
+        
+        //public string getName()
+        //{
+        //    return firstName;
+        //    return lastName;
+        //    return suffix;
+        //    return midIn;
+        //}
+
+        //public string fullName(string lastName, string firstName, string suffix, string midIn);
+    }
+
+}
